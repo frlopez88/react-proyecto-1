11cff9218779f2b94df1256dc2ee690caf036e04
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-05-03 20:56:35 -0600

    Commit de Contador

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+
+export const App = () => {
+
+  const [contador, setContador] = useState(0)
+
+  function onClickHandler(val) {
+    setContador(contador + val)
+  }
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+
+      <p>{contador}</p>
+
+      <button onClick={() => onClickHandler(1)} >+</button>
+      <button onClick={() => onClickHandler(-1)}>-</button>
+
+    </>
+  )
+}
+
